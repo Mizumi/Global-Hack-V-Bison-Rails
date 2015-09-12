12622da3fcee96a14aee696d7ae42c8cdd856371
Brandon Sanders <brandon@mechakana.com>
2015-09-12 14:58:51 -0500

    Wonderfulness!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery.min
 //= require turbolinks
-//= require turbolinks_transitions
 //= require bootstrap.min
 //= require_tree .
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,21 @@
 class TicketsController < ApplicationController
 	def dispute
-		if params[:ticket_id] && session[:ticket] &&
-		   session[:ticket]["citationNumber"].to_s == params[:ticket_id].to_s
-			@ticket = session[:ticket]
+		if params[:ticket]
+			query = Global.api + "Tickets/CitationNumber/" + params[:ticket].to_s
+			response = RestClient.get(query)
+			@ticket = JSON.parse(response.body)
+
+			query = Global.api + "Violation/CitationNumber/" + params[:ticket].to_s
+			response = RestClient.get(query)
+			@violations = JSON.parse(response.body)
+
+			@violationFines = 0
+			@violationCourtFees = 0
+			@violations.each do |v|
+				@violationFines += v["fineAmount"].to_f
+				@violationCourtFees += v["courtCost"].to_f
+			end
+
 			render "dispute"
 		else
 			render "find"
@@ -10,9 +23,22 @@ class TicketsController < ApplicationController
 	end
 
 	def pay
-		if params[:ticket_id] && session[:ticket] &&
-		   session[:ticket]["citationNumber"].to_s == params[:ticket_id].to_s
-			@ticket = session[:ticket]
+		if params[:ticket]
+			query = Global.api + "Tickets/CitationNumber/" + params[:ticket].to_s
+			response = RestClient.get(query)
+			@ticket = JSON.parse(response.body)
+
+			query = Global.api + "Violation/CitationNumber/" + params[:ticket].to_s
+			response = RestClient.get(query)
+			@violations = JSON.parse(response.body)
+
+			@violationFines = 0
+			@violationCourtFees = 0
+			@violations.each do |v|
+				@violationFines += v["fineAmount"].to_f
+				@violationCourtFees += v["courtCost"].to_f
+			end
+
 			render "pay"
 		else
 			render "find"
@@ -35,10 +61,8 @@ class TicketsController < ApplicationController
 		response = RestClient.get(query)
 
 		if (response.body != "[]")
-			@ticket = JSON.parse(response.body)
-			session[:ticket] = @ticket
-
-			@ticket.each do |t|
+			@tickets = JSON.parse(response.body)
+			@tickets.each do |t|
 				response = RestClient.get(Global.api + "Violation/CitationNumber/" + t["citationNumber"].to_s)
 				t["violations"] = JSON.parse(response.body)
 			end
